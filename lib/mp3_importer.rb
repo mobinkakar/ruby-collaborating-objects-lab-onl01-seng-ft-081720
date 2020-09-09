3579db295749f681b0b017ff833acb53912abd7c
Mobin Kakar <mobin.kakar@wsabronx.org>
2020-09-09 15:12:47 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,17 +1,21 @@
 class MP3Importer
 
-  attr_accessor :path
+    attr_accessor :path
 
-  def initialize(path)
-    @path = path
-  end
+    def initialize(path)
+        @path = path
+    end
 
-  def files
-    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
-  end
+    def files
+  
+        @files || = Dir.glob("#{path}/*.mp3").collect { | file | file.gsub("#{path}/", "") }
 
-  def import
-    files.each {|song| Song.new_by_filename(song)}
-  end
+
+        # @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
+    end
+
+    def import
+        files.each { | file | Song.new_by_filename(file) }
+    end
 
 end
